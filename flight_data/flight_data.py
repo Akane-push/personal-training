621bdd73a3eee074f2569d8cc2f36bcabd69337d
Nagashka <edr.potdevin@gmail.com>
2026-03-30 19:00:29 +0200

    Airport_list in another file, data files generate with report date

--- flight_data/flight_data.py
+++ flight_data/flight_data.py
@@ -1,17 +1,26 @@
 import sys
 import os
+from datetime import datetime
 import time
 import requests
 import pandas as pd
-sys.path.append(os.path.join(os.path.dirname(__file__), "..", "id"))
+import json
+
+#Loading required files
+current_folder = os.path.dirname(__file__)
+sys.path.append(os.path.join(current_folder, "..", "id"))
 from identification import LufthansaAPI
+airports_json = os.path.join(current_folder, "..", "reference_data", "study_airport.json")
+with open(airports_json, 'r', encoding='utf-8') as f:
+    airports_list = json.load(f)
 
 datas_path = os.getenv("Datas_path")
 limit_call_per_hour = 1000
-airports_list = ["CDG", "FRA"]
+
 
 class LufthansaFly:
     def __init__(self):
+        #Identification
         self.api = LufthansaAPI()
         if self.api.token is None:
             self.token = self.api.get_token()
@@ -20,11 +29,15 @@ class LufthansaFly:
         self.headers = {'Authorization': f'Bearer {self.token}'}
 
     # Get landed flights informations
-    def get_flights(self):
+    def get_flights(self, date: str):
+        """
+        date = 'AAAA-MM-DDTHH:MM' (Convert to ISO 8601 format with 'T' separator)
+        """
+        self.date = date
         url = f"{self.api.url}/operations/customerflightinformation/arrivals"
         df_flight_list = pd.DataFrame()
         for airport in airports_list:
-            data_json = requests.get(f"{url}/{airport}/2026-03-28T14:00?limit=20", headers=self.headers).json().get('FlightInformation', {}).get('Flights', {}).get('Flight', [])
+            data_json = requests.get(f"{url}/{airport}/{date}?limit=100", headers=self.headers).json().get('FlightInformation', {}).get('Flights', {}).get('Flight', [])
             filtered_json = [data for data in data_json if data["Arrival"]["Status"]["Description"] == "Flight Landed"]
             df_list = pd.DataFrame({
                     'Flight_Number': [f"{flight['OperatingCarrier']['AirlineID']}{flight['OperatingCarrier']['FlightNumber']}" for flight in filtered_json],
@@ -45,16 +58,20 @@ class LufthansaFly:
             time.sleep(6)
         return df_flight_list
 
-    def get_datas(self):
-        df_flight_list = self.get_flights()
+    #Generate the files
+    def get_datas(self, date: str):
+        df_flight_list = self.get_flights(date)
 
-        name_data_file = "datas.parquet"
+        name_data_file = f"{date.split('T')[0]}_flydatas.parquet"
         file_path = os.path.join(datas_path, name_data_file)
-        #os.makedirs(os.path.dirname(file_path), exist_ok=True)
 
         df_flight_list.to_parquet(file_path, engine="pyarrow",  index=False)
         print(f"[INFO] Datas are available in the: {file_path} file !")
 
+    #Existing file verification
+    #def save_datas(self):
+
+
 
 if __name__ == "__main__":
-    LufthansaFly().get_datas()
+    LufthansaFly().get_datas("2026-03-28T14:00")
